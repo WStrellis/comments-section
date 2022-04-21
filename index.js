91c0c5c3e32384d01442b71bc2d7a676d48d644b
Westley Strellis <wstrellis@gmail.com>
2022-04-20 21:00:14 -0400

    switch to apollo

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import { join } from "path"
+import { ApolloServer } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import http from 'http';
 import { loadSchemaSync } from "@graphql-tools/load"
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader"
 //import { addResolversToSchema } from "@graphql-tools/schema"
-import { createServer } from "@graphql-yoga/node"
-
 import express from "express"
+import resolvers from './graphql/resolvers.js'
+
 
 const schema = loadSchemaSync(join("graphql","schema.graphql"), {
     loaders: [new GraphQLFileLoader()],
@@ -12,16 +15,23 @@ const schema = loadSchemaSync(join("graphql","schema.graphql"), {
 
 // const schemaWithResolvers = addResolversToSchema({ schema, resolvers })
 
-const app = express()
-
-const yoga = createServer({
-    schema,
-    // schema: schemaWithResolvers,
-    graphiql: true,
-})
-
-app.use("/graphql", yoga)
-
 const PORT = process.env.HTTP_PORT || 3000
 
-app.listen(PORT, () => console.log("App listening on port", PORT))
+async function startApollo(typeDefs,resolvers){
+    const app = express()
+    const httpServer = http.createServer(app)
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer})]
+    })
+    await server.start()
+    server.applyMiddleware({app})
+    httpServer.listen(PORT, () => console.log("App listening on port", PORT))
+}
+
+startApollo(schema,resolvers)
+
+
+
+
