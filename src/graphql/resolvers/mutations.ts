a742ee65759e692b0e29f0969d425e027bebdc52
Westley Strellis <wstrellis@gmail.com>
2022-05-15 13:10:19 -0400

    can add a user with graphql

--- src/graphql/resolvers/mutations.ts
+++ src/graphql/resolvers/mutations.ts
@@ -1,17 +1,41 @@
 import type { User, AddUserResponse } from "../../types/index"
+import { type InsertOneResult, ObjectId } from "mongodb"
+import { getErrorMessage } from "../../util/errors"
 
 export default {
-    // addUser: (
-    //     _: any,
-    //     { name }: { name: string },
-    //     ctx: any,
-    // ): AddUserResponse => {
-    // const newUser: User = { id: ctx.db.users.length + 1, name }
-    // ctx.db.users.push(newUser)
-    // return {
-    //     success: true,
-    //     message: "new user created",
-    //     user: newUser,
-    // }
-    // },
+    addUser: async (
+        _: any,
+        { name }: { name: string },
+        ctx: any,
+    ): Promise<AddUserResponse> => {
+        // add user
+        const res: AddUserResponse = {
+            success: true,
+            message: "",
+            user: undefined,
+        }
+        try {
+            const dbRes: InsertOneResult =
+                await ctx.dataSources.usersClx.addUser(name)
+
+            res.success = dbRes.acknowledged
+
+            if (dbRes.acknowledged) {
+                const user: User = {
+                    _id: dbRes.insertedId.toString(),
+                    name,
+                }
+                res.user = user
+                res.message = "Created user " + name
+            } else {
+                throw new Error(
+                    `Could not create user: ${JSON.stringify(dbRes)}`,
+                )
+            }
+        } catch (error) {
+            res.message = getErrorMessage(error)
+            res.success = false
+        }
+        return res
+    },
 }
